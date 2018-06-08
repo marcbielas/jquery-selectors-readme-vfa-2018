43c5e1e9a61db58da25352cfaeab80875eabd421
Marc Bielas <marc@bielas.com>
2018-06-08 19:46:55 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+
+paragraphSelector = function() {
+  return $('p');
+}
+
+lastImageSelector = function() {
+  return $('img:last');
+}
+
+ninjaBabySelector = function() {
+  return $('#baby-ninja');
+}
+
+divSelector = function() {
+  return $('div.pics');
+}
+
+firstListItem = function() {
+  return $('#pic-list:first-child');
+}
